6d65ba19abb5ffb335ce72051d88089269df4318
RticRtic <90681250+RticRtic@users.noreply.github.com>
2022-04-25 15:11:16 +0200

    searching with api works

--- src/components/homepage/SearchInput.jsx
+++ src/components/homepage/SearchInput.jsx
@@ -1,16 +1,44 @@
 import "../../styles/searchInput.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { getSearchedApi } from "../../models/apiSearchInput";
+import { useEffect, useState } from "react";
 
 const SearchInput = () => {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+   getSearchedApi(input);
+  }, [input]);
+
+  
+
+  const clearInput = () => {
+    setInput("");
+  }
+
   return (
     <div>
-        <header className="header-container">
-            <h2 className="header"> a paradise for movie collectors.</h2>
-        </header>
+      <header className="header-container">
+        <h2 className="header"> a paradise for movie collectors.</h2>
+      </header>
       <div className="searchbar-input-container">
+        <div className="magnifyingglass-container">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="magnifyingglass"
+          />
+        </div>
         <input
           placeholder="Search movie..."
           className="search-input-field"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          
         ></input>
+        {/* <div className="magnifyingglass-container">
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="magnifyingglass"  />
+        </div> */}
       </div>
     </div>
   );
--- src/models/apiSearchInput.js
+++ src/models/apiSearchInput.js
@@ -1,2 +1,15 @@
+import { faTeeth } from "@fortawesome/free-solid-svg-icons"
 
+const getSearchedApi = async (input) => {
 
+//let url = "https://api.themoviedb.org/3/search/movie?api_key=478482cb8ce7c6d6fa5ecb5d066f3fff&language=pt-EN&query={INPUT}"
+let searchApi = [];
+
+const respone = await fetch (`https://api.themoviedb.org/3/search/movie?api_key=478482cb8ce7c6d6fa5ecb5d066f3fff&language=pt-EN&query=${input}`);
+const data = await respone.json();
+searchApi.push(data);
+console.log(searchApi);
+
+}
+
+export {getSearchedApi}
