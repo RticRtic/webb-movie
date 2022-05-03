75595bd372312c9a7a06fcf4ce281e19b10881a7
RticRtic <90681250+RticRtic@users.noreply.github.com>
2022-05-03 14:28:37 +0200

    .

--- src/components/homepage/SearchInput.jsx
+++ src/components/homepage/SearchInput.jsx
@@ -2,9 +2,12 @@ import "../../styles/searchInput.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { getSearchedApi } from "../../models/apiSearchInput";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { actions, STATUS } from "../../features/searchMovie";
+import { STATUS } from "../../features/searchMovie";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const SearchInput = () => {
   const status = useSelector((state) => state.searchedMovie.status);
@@ -21,14 +24,15 @@ const SearchInput = () => {
     content = "";
   } else if (status === STATUS.SUCCESS) {
     content = movie.results.map((movie) => (
-      <div className="movieItem">
-        <div key={movie.id} className="movieTitle">{movie.title}</div>
+      <div key={movie.id} className="movieItem">
         <img
           className="movie-img"
           src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
           alt="img"
         />
-        <i className="vote-average">{movie.vote_average}</i>
+        <div className="vote-average-container">
+          <i className="vote-average">{movie.vote_average}</i>
+        </div>
       </div>
     ));
     console.log("movies: ", movie);
@@ -70,9 +74,51 @@ const SearchInput = () => {
         <FontAwesomeIcon icon={faMagnifyingGlass} className="magnifyingglass"  />
         </div> */}
       </div>
-      <div className="movieList">{content}</div>
+      <div>
+        <div className="movieList">{content}</div>
+      </div>
     </div>
   );
 };
+
+{
+  /* <div className="trending_container">
+            <header>Trending Movies</header>
+            <div>
+
+                
+
+                <Swiper
+                className={'swiper-container'}
+
+                slidesPerView={'auto'}
+                spaceBetween={10}
+                direction={"horizontal"}
+                onInit={(ev) => {
+                    set_my_swiper(ev)
+                }}
+                >
+                    
+                    <button className={('trending_pagination_button') + (' left')} onClick={() => { my_swiper.slidePrev() }}>
+                        <FontAwesomeIcon icon={faChevronLeft}/>
+                        </button>
+                    
+                    {trending.data.results.map(movie => (
+                        <SwiperSlide key={movie.id} className='swiper-slide'>
+                            <TrendingMovieCard movie={movie}/>
+                        </SwiperSlide>
+                    ))}
+                    
+                    <button className={('trending_pagination_button') + (' right')} onClick={() => { my_swiper.slideNext() }}>
+                        <FontAwesomeIcon icon={faChevronRight}/>
+                        </button>
+
+            </Swiper>
+
+
+            </div>
+            
+        </div> */
+}
 
 export default SearchInput;
--- src/models/apiSearchInput.js
+++ src/models/apiSearchInput.js
@@ -9,7 +9,7 @@ const getSearchedApi = async (input, dispatch) => {
   try {
     dispatch(actions.isFetching());
     const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=478482cb8ce7c6d6fa5ecb5d066f3fff&language=pt-EN&query=${input}`
+      `https://api.themoviedb.org/3/search/movie?api_key=478482cb8ce7c6d6fa5ecb5d066f3fff&language=en-EN&query=${input}`
     );
     const data = await response.json();
     dispatch(actions.success(data));
