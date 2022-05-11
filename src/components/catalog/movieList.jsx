7fef5bf35ebb11601e63a9ce6bca5a50f1395edd
RticRtic <90681250+RticRtic@users.noreply.github.com>
2022-05-11 12:30:14 +0200

    updated css in movieList.css

--- src/components/catalog/movieList.jsx
+++ src/components/catalog/movieList.jsx
@@ -1,62 +1,38 @@
 import { useState } from "react";
 import "../../styles/movieList.css";
 
-import { faStar, faShoppingCart, faDollarSign } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faStar,
+  faShoppingCart,
+  faDollarSign,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MovieList = ({ actionMovieData, dramaMovieData }) => {
   const actionMovies = actionMovieData.map((movie, index) => (
     <div key={index}>
       <img
-        className="action-movie-img"
+        className="movie-img"
         src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
         alt=""
       />
-       <div className="action-shoppingcart-container">
-        <i className="action-icon-shoppingcart"><FontAwesomeIcon icon={faShoppingCart}/></i>
+      <div className="shoppingcart-container">
+        <i className="icon-shoppingcart">
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </i>
       </div>
-      <div className="action-movie-vote_average-container">
-        <p className="action-movie-vote_average">
-          <FontAwesomeIcon className="action-icon-star" icon={faStar} />
-          {movie.vote_average}
-        </p>
-      </div>
-      <div className="action-movie_price-container">
-        <p className="action-movie-price">
-          <FontAwesomeIcon className="action-icon-price" icon={faDollarSign} />
-          99.9
-        </p>
-      </div>
-      <div className="action-movie-title-release-date-container">
-        <article className="action-movie-title-release-date">
-          {movie.title}
-          <br />
-          {movie.release_date}
-        </article>
-      </div>
-    </div>
-   
-  ));
-
-  const dramaMovies = dramaMovieData.map((movie, index) => (
-    <div key={index}>
-      <img
-        className="drama-movie-img"
-        src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-        alt=""
-      />
-
       <div className="movie-vote_average-container">
-        <i className="movie-vote_average">
+        <p className="movie-vote_average">
           <FontAwesomeIcon className="icon-star" icon={faStar} />
           {movie.vote_average}
-        </i>
+        </p>
       </div>
       <div className="movie_price-container">
-        <i className="movie-price">
+        <p className="movie-price">
           <FontAwesomeIcon className="icon-price" icon={faDollarSign} />
           99.9
-        </i>
+        </p>
       </div>
       <div className="movie-title-release-date-container">
         <article className="movie-title-release-date">
@@ -65,6 +41,41 @@ const MovieList = ({ actionMovieData, dramaMovieData }) => {
           {movie.release_date}
         </article>
       </div>
+    </div>
+  ));
+
+  const dramaMovies = dramaMovieData.map((movie, index) => (
+    <div key={index}>
+      <img
+        className="movie-img"
+        src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+        alt=""
+      />
+      <div className="shoppingcart-container">
+        <i className="icon-shoppingcart">
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </i>
+      </div>
+      <div className="movie-vote_average-container">
+        <p className="movie-vote_average">
+          <FontAwesomeIcon className="icon-star" icon={faStar} />
+          {movie.vote_average}
+        </p>
+      </div>
+      <div className="movie_price-container">
+        <p className="movie-price">
+          <FontAwesomeIcon className="icon-price" icon={faDollarSign} />
+          99.9
+        </p>
+      </div>
+      <div className="movie-title-release-date-container">
+        <article className="movie-title-release-date">
+          {movie.title}
+          <br />
+          {movie.release_date}
+        </article>
+      </div>
+      
     </div>
   ));
 
