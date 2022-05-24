6b5592ffe40964ee3a719d3e913530936da536e8
RticRtic <90681250+RticRtic@users.noreply.github.com>
2022-05-24 11:11:38 +0200

    change page with api

--- src/components/catalog/movieList.jsx
+++ src/components/catalog/movieList.jsx
@@ -3,29 +3,12 @@ import "../../styles/movieList.css";
 // import "../../styles/movie-card.css";
 import MovieCard from "../globals/movieCard";
 
-import {
-  faStar,
-  faShoppingCart,
-  faDollarSign,
-  faArrowRight,
-} from "@fortawesome/free-solid-svg-icons";
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
   import { apiMovies } from "../../models/apiCatalog";
 
 
 
 const MovieList = ({ movieData }) => {
-  const [apiPage, setApiPage] = useState(1);
-  // const [movieList, setMoveList] = useState([]);
-
-  // const handleIncreaseApiPage = (genre, page) => {
-  //   setApiPage(apiPage + 1);
-  //   apiMovies(setMoveList, genre, page)
-   
-  
-  // };
-
-  
   const movies = movieData.map((movie) => (
     
     <MovieCard movie={movie} key={movie.id} />
@@ -38,14 +21,6 @@ const MovieList = ({ movieData }) => {
   return (
     <div className="movieList-component">
       {movies}
-
-      <div className="arrow-container">
-        <i className="arrow-right">
-          <FontAwesomeIcon icon={faArrowRight}
-          style={{color : "white"}}
-          />
-        </i>
-      </div>
     </div>
   );
 };
